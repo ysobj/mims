4ed1280b883cc2457f858ccab24f036418cb1fd4
ysobj <litailang+ysobj@gmail.com>
2015-05-17 23:48:45 +0900

    first commit

---
+++ src/main/java/com/ysobj/mims/HelloWorldApplication.java
@@ -0,0 +1,53 @@
+package com.ysobj.mims;
+
+import org.skife.jdbi.v2.DBI;
+
+import com.ysobj.mims.health.TemplateHealthCheck;
+import com.ysobj.mims.resources.HelloWorldResource;
+
+import io.dropwizard.Application;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.jdbi.DBIFactory;
+import io.dropwizard.migrations.MigrationsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+	public static void main(String[] args) throws Exception {
+		new HelloWorldApplication().run(args);
+	}
+
+	@Override
+	public String getName() {
+		return "hello-world";
+	}
+
+	@Override
+	public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+		bootstrap.addBundle(new MigrationsBundle<HelloWorldConfiguration>() {
+			public DataSourceFactory getDataSourceFactory(
+					HelloWorldConfiguration configuration) {
+				return configuration.getDataSourceFactory();
+			}
+		});
+	}
+
+	@Override
+	public void run(HelloWorldConfiguration configuration,
+			Environment environment) throws Exception {
+		final HelloWorldResource resource = new HelloWorldResource(
+				configuration.getTemplate(), configuration.getDefaultName());
+		final TemplateHealthCheck healthCheck = new TemplateHealthCheck(
+				configuration.getTemplate());
+
+		final DBIFactory factory = new DBIFactory();
+		final DBI jdbi = factory.build(environment,
+				configuration.getDataSourceFactory(), "postgresql");
+		// final UserDAO dao = jdbi.onDemand(UserDAO.class);
+		// environment.jersey().register(new UserResource(dao));
+
+		environment.healthChecks().register("template", healthCheck);
+		environment.jersey().register(resource);
+	}
+
+}
---
+++ src/main/java/com/ysobj/mims/HelloWorldConfiguration.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.ysobj.mims;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
+/**
+ * @author ayataro
+ *
+ */
+public class HelloWorldConfiguration extends Configuration {
+	@NotEmpty
+	private String template;
+
+	@NotEmpty
+	private String defaultName = "Stranger";
+
+	@Valid
+	@NotNull
+	private DataSourceFactory database = new DataSourceFactory();
+
+	@JsonProperty("database")
+	public DataSourceFactory getDataSourceFactory() {
+		return database;
+	}
+
+	@JsonProperty("database")
+	public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+		this.database = dataSourceFactory;
+	}
+
+	@JsonProperty
+	public String getTemplate() {
+		return template;
+	}
+
+	@JsonProperty
+	public void setTemplate(String template) {
+		this.template = template;
+	}
+
+	@JsonProperty
+	public String getDefaultName() {
+		return defaultName;
+	}
+
+	@JsonProperty
+	public void setDefaultName(String name) {
+		this.defaultName = name;
+	}
+}
